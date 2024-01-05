30ad9f6b02224d0d1b1d72e387dd7d99cd249b68
HannesNyfelt <hannes.nyfelt@gmail.com>
2024-01-05 10:43:22 +0200

    Stuff

---
+++ src/app/component/Home.tsx
@@ -0,0 +1,46 @@
+import { Box, Typography } from '@mui/material'
+
+export default function Home() {
+    return (
+        <Box>
+            <Box>
+                <Typography variant='h2'>Welcome to Optima</Typography>
+            </Box>
+
+            <Box>
+                <Typography variant='h4'>Info om Optima</Typography>
+                <Typography variant='subtitle1'>
+                    Optima samkommuns ägarkommuner är Jakobstad, Karleby, Kronoby, Larsmo, Nykarleby, Pedersöre och Vörå. Samkommunen ordnar via sin utbildningsenhet Optima yrkesutbildning för unga och vuxna, ansvarar för yrkesutbildning med krävande särskilt stöd i Svenskfinland samt erbjuder olika former av tjänster för arbetslivet.
+
+                    Vi verkar i första hand i Jakobstadsregionen i Österbotten men inom yrkesinriktad utbildning med krävande särskilt stöd verkar vi i hela Svenskfinland.. Våra adresser är:
+
+                    <Typography variant='body2'>Trädgårdsgatan 30, Jakobstad</Typography>
+                    <Typography variant='body2'>Vasavägen 149, Jakobstad</Typography>
+
+                    <Typography variant='body2'>Lannäslund 1, Jakobstad</Typography>
+
+                    <Typography variant='body2'>Åminnevägen 2A, Nykarleby.</Typography>
+
+                    <Typography variant='body2'>Strandvägen 1, 21600 Pargas</Typography>
+
+                    <Typography variant='body2'>Kungsgårdsvägen 21 A, Vasa</Typography>
+
+                    <Typography variant='body2'>Arabiagatan 12, Helsingfors</Typography>
+
+                    <Typography variant='body2'>Brunnsgatan 37, Borgå</Typography>
+
+
+                    <Typography variant='body2'>
+
+                        Utbildningarna är i huvudsak svenskspråkiga.
+                        Arbetslivstjänsterna är naturligtvis tvåspråkiga, likaså fest-, seminarie-, restaurang- och cateringverksamheten.
+                        FO-nummer: 0796234-1
+                    </Typography>
+
+
+
+                </Typography>
+            </Box>
+        </Box>
+    )
+}
